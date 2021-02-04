f6c6278e0c90dc2d5f41e44bca8670bbb7218a3f
karlG <kallegunnarsson@hotmail.com>
2021-02-05 00:39:41 +0100

    Added some more countries, I've done 20 in total. Also made it possible for user to use enter-btn and added some current score info

--- readMe.md
+++ readMe.md
@@ -1,2 +1,9 @@
 # African Capitals Quiz
 
+## By Karl Gunnarsson
+
+### Type of project
+Game
+
+### Concept/rules
+You are given the name and the flag of the country. Your objective is to write the name of the capital.
--- script.js
+++ script.js
@@ -29,10 +29,138 @@ const questionsArr = [
         flagURL: 'img/flags/algeria.png',
         nr: 4,
     },
+    {
+        country: 'Chad',
+        capitalName: "N'Djamena",
+        correctAnswers: ["n'djamena", "ndjamena", "endjamena", "n djamena", "en djamena"],
+        flagURL: 'img/flags/chad.png',
+        nr: 5,
+    },
+    {
+        country: 'Nigeria',
+        capitalName: "Abuja",
+        correctAnswers: ["abuja", "aboja", "abouja", "abouya", "abuya", "aboya"],
+        flagURL: 'img/flags/nigeria.png',
+        nr: 6,
+    },
+    {
+        country: 'Ethiopia',
+        capitalName: "Addis Ababa",
+        correctAnswers: ["addis ababa", "addis abeba", "adis ababa", "adis abbaba", "adis abbeba"],
+        flagURL: 'img/flags/ethiopia.png',
+        nr: 7,
+    },
+    {
+        country: 'Democratic Republic of the Congo',
+        capitalName: "Kinshasa",
+        correctAnswers: ["kinshasa", "kinchasa"],
+        flagURL: 'img/flags/democraticRepublicOfTheCongo.png',
+        nr: 8,
+    },
+    {
+        country: 'Tanzania',
+        capitalName: "Dodoma",
+        correctAnswers: ["dodoma", "dodouma"],
+        flagURL: 'img/flags/tanzania.png',
+        nr: 9,
+    },
+    {
+        country: 'Kenya',
+        capitalName: "Nairobi",
+        correctAnswers: ["nairobi", "najrobi"],
+        flagURL: 'img/flags/kenya.png',
+        nr: 10,
+    },
+    {
+        country: 'Uganda',
+        capitalName: "Kampala",
+        correctAnswers: ["kampala", "campala"],
+        flagURL: 'img/flags/uganda.png',
+        nr: 11,
+    },
+    {
+        country: 'Sudan',
+        capitalName: "Khartoum",
+        correctAnswers: ["khartoum", "kartoum", "kahrtoum", "cartoum"],
+        flagURL: 'img/flags/sudan.png',
+        nr: 12,
+    },
+    {
+        country: 'Morocco',
+        capitalName: "Rabat",
+        correctAnswers: ["rabat", "rabath", "rabbat", "rabatt", "rabbatt"],
+        flagURL: 'img/flags/morocco.png',
+        nr: 13,
+    },
+    {
+        country: 'Mozambique',
+        capitalName: "Maputo",
+        correctAnswers: ["maputo", "maputho", "mapputo"],
+        flagURL: 'img/flags/mozambique.png',
+        nr: 14,
+    },
+    {
+        country: 'Ghana',
+        capitalName: "Accra",
+        correctAnswers: ["accra", "acra", "akra"],
+        flagURL: 'img/flags/ghana.png',
+        nr: 15,
+    },
+    {
+        country: 'Angola',
+        capitalName: "Luanda",
+        correctAnswers: ["luanda", "luadna"],
+        flagURL: 'img/flags/angola.png',
+        nr: 16,
+    },
+    {
+        country: 'Somalia',
+        capitalName: "Mogadishu",
+        correctAnswers: ["mogadishu", "mogadihsu", "mogadichu"],
+        flagURL: 'img/flags/somalia.png',
+        nr: 17,
+    },
+    {
+        country: 'Ivory Coast',
+        capitalName: "Yamoussoukro",
+        correctAnswers: ["yamoussoukro", "yamosokro", "jamousoukro", "yamousoukro"],
+        flagURL: 'img/flags/ivoryCoast.png',
+        nr: 18,
+    },
+    {
+        country: 'Madagascar',
+        capitalName: "Antananarivo",
+        correctAnswers: ["antananarivo", "antanarivo", "antanananarivo"],
+        flagURL: 'img/flags/madagascar.png',
+        nr: 19,
+    },
+    {
+        country: 'Cameroon',
+        capitalName: "Yaoundé",
+        correctAnswers: ["yaoundé", "yaounde", "yaonde", "yaunde",],
+        flagURL: 'img/flags/cameroon.png',
+        nr: 20,
+    },
 ];
+
+
+
+
+
+
+
+
+
+/* =================
+    INITIAL VALUES start
+   =================
+*/
 
 //This value increases with 1 when user press next-btn
 let questionsArrIndex = 0;
+
+//1 is added per correct answer
+let score = 0;
 
 //Give initial value to these. Change to empty str or something, if you make order random later
 let currentCountry = questionsArr[questionsArrIndex].country;
@@ -40,6 +168,19 @@ let currentCapital = questionsArr[questionsArrIndex].capitalName;
 
 //Temp boolean that applies for the current question
 let isCorrect = false;
+
+/* =================
+    INITIAL VALUES end
+   =================
+*/
+
+
+
+
+
+
+
+
 
 /* =================
     QUERY SELECTORS start
@@ -52,7 +193,20 @@ const answerInput = document.querySelector('.answer-input');
 const correctMsg = document.querySelector('.correct-message');
 const wrongMsg = document.querySelector('.wrong-message');
 const currentFlag = document.querySelector('.flag');
+const infoScore = document.querySelector('.info-score');
+const infoQuestion = document.querySelector('.info-question');
+const infoQuestionsLeft = document.querySelector('.info-questions-left');
 
+/* =========== Some more initial values of variables ===========*/
+
+//Render 0 as current score
+infoScore.innerHTML = score;
+
+//Render 54 as questions left
+infoQuestionsLeft.innerHTML = '54';
+
+//Render 1/54 as question number
+infoQuestion.innerHTML = '1/54';
 
 //Make the placeholder to first Country
 answerInput.placeholder = currentCountry;
@@ -63,18 +217,45 @@ answerInput.placeholder = currentCountry;
 */
 
 
+
+
+
+
+
+
+
 /* =================
     EVENT LISTENERS start
    =================
 */
 
 doneBtn.addEventListener('click', checkAnswer)
+
+/* Enable enter-btn START */
+answerInput.addEventListener('keyup', event => {
+    //if nextBtn is not visible = run checkAnswer
+    if (event.keyCode === 13 && nextBtn.style.display !== 'inline') {
+        checkAnswer();
+        //if nextBtn is visible = run nextQuestion
+    } else if (event.keyCode === 13 && nextBtn.style.display === 'inline') {
+        nextQuestion();
+    }
+})
+/* Enable enter-btn END */
+
 nextBtn.addEventListener('click', nextQuestion)
 
 /* =================
     EVENT LISTENERS end
    =================
 */
+
+
+
+
+
+
+
 
 /* =================
     FUNCTIONS start
@@ -90,26 +271,27 @@ function checkAnswer() {
     answerFeedback(currentAnswer);
 }
 
-function answerFeedback (answer) {
-    
+function answerFeedback(answer) {
+
     for (let i = 0; i < questionsArr[questionsArrIndex].correctAnswers.length; i++) {
         if (answer === questionsArr[questionsArrIndex].correctAnswers[i]) {
             isCorrect = true;
-            resultMessage();
-            }
-        }
-        if (isCorrect === false) {
+            infoScore.innerHTML = score += 1;
             resultMessage();
         }
     }
+    if (isCorrect === false) {
+        resultMessage();
+    }
+}
 
-function resultMessage () {
+function resultMessage() {
     if (isCorrect === true) {
         correctMsg.innerHTML = `Correct! The capital of ${currentCountry} is <b> ${currentCapital}<b/>.`;
     } else {
         wrongMsg.innerHTML = `Wrong, the capital of ${currentCountry} is <b> ${currentCapital}<b/>.`;
     }
-    
+
     //Hide check-btn
     doneBtn.style.display = 'none';
 
@@ -118,6 +300,13 @@ function resultMessage () {
 };
 
 function nextQuestion() {
+    //Updates user info (questions left)
+    let qLeft = 54 - (questionsArrIndex + 1);
+    infoQuestionsLeft.innerHTML = qLeft.toString();
+
+    //Updates user info (question-nr)
+    infoQuestion.innerHTML = (questionsArrIndex + 1) + 1 + '/54';
+
     //Add 1 to arrIndex + go to next country/object
     questionsArrIndex++;
     currentCountry = questionsArr[questionsArrIndex].country;
